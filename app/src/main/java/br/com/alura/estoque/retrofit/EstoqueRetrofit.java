de312955900ee564c0cfe0d9d8356cf4f9b30933
Leonardo Magno Marones Lopes <leonardo.marones@mobicare.com.br>
2021-06-21 16:05:28 -0300

    - Integrando comportamentos de inserção, edição e remoção.

--- app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -1,6 +1,6 @@
 package br.com.alura.estoque.retrofit;
 
-import java.util.logging.Logger;
+import org.jetbrains.annotations.NotNull;
 
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import okhttp3.OkHttpClient;
@@ -10,22 +10,29 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EstoqueRetrofit {
 
+    public static final String URL_BASE = "http://192.168.0.171:8080/";
     private final ProdutoService produtoService;
 
     public EstoqueRetrofit() {
-        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(logging)
-                .build();
+        OkHttpClient client = configuraClient();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.0.171:8080/")
+                .baseUrl(URL_BASE)
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         produtoService = retrofit.create(ProdutoService.class);
+    }
+
+    @NotNull
+    private OkHttpClient configuraClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+        return client;
     }
 
     public ProdutoService getProdutoService() {
--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -1,36 +1,34 @@
 package br.com.alura.estoque.ui.activity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Toast;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.IOException;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 import br.com.alura.estoque.R;
-import br.com.alura.estoque.asynctask.BaseAsyncTask;
-import br.com.alura.estoque.database.EstoqueDatabase;
-import br.com.alura.estoque.database.dao.ProdutoDAO;
 import br.com.alura.estoque.model.Produto;
 import br.com.alura.estoque.repository.ProdutoRepository;
-import br.com.alura.estoque.retrofit.EstoqueRetrofit;
-import br.com.alura.estoque.retrofit.service.ProdutoService;
 import br.com.alura.estoque.ui.dialog.EditaProdutoDialog;
+import br.com.alura.estoque.ui.dialog.FormularioProdutoDialog;
 import br.com.alura.estoque.ui.dialog.SalvaProdutoDialog;
 import br.com.alura.estoque.ui.recyclerview.adapter.ListaProdutosAdapter;
-import retrofit2.Call;
-import retrofit2.Response;
 
 public class ListaProdutosActivity extends AppCompatActivity {
 
     private static final String TITULO_APPBAR = "Lista de produtos";
+    private static final String MENSAGEM_ERRO_BUSCA_PRODUTOS = "Não foi possível carregar os produtos novos";
+    private static final String MENSAGEM_ERRO_REMOCAO = "Não foi possível remover o produto";
+    private static final String MENSAGEM_ERRO_SALVA = "Não foi possível salvar o produto";
+    private static final String MENSAGEM_ERRO_EDICAO = "Não foi possível editar o produto";
     private ListaProdutosAdapter adapter;
-    private ProdutoDAO dao;
+    private ProdutoRepository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +38,22 @@ public class ListaProdutosActivity extends AppCompatActivity {
 
         configuraListaProdutos();
         configuraFabSalvaProduto();
+        repository = new ProdutoRepository(this);
+        buscaProdutos();
+    }
 
-        EstoqueDatabase db = EstoqueDatabase.getInstance(this);
-        dao = db.getProdutoDAO();
+    private void buscaProdutos() {
+        repository.buscaProdutos(new ProdutoRepository.DadosCarregadosCallback<List<Produto>>() {
+            @Override
+            public void quandoSucesso(List<Produto> produtosNovos) {
+                adapter.atualiza(produtosNovos);
+            }
 
-        ProdutoRepository repository = new ProdutoRepository(dao);
-        repository.buscaProdutos(adapter::atualiza);
+            @Override
+            public void quandoFalha(String erro) {
+                mostraErro(MENSAGEM_ERRO_BUSCA_PRODUTOS);
+            }
+        });
     }
 
     private void configuraListaProdutos() {
@@ -55,13 +63,22 @@ public class ListaProdutosActivity extends AppCompatActivity {
         adapter.setOnItemClickRemoveContextMenuListener(this::remove);
     }
 
-    private void remove(int posicao,
-                        Produto produtoRemovido) {
-        new BaseAsyncTask<>(() -> {
-            dao.remove(produtoRemovido);
-            return null;
-        }, resultado -> adapter.remove(posicao))
-                .execute();
+    private void remove(int posicao, Produto produtoEscolhido) {
+        repository.remove(produtoEscolhido, new ProdutoRepository.DadosCarregadosCallback<Void>() {
+            @Override
+            public void quandoSucesso(Void resultado) {
+                adapter.remove(posicao);
+            }
+
+            @Override
+            public void quandoFalha(String erro) {
+                mostraErro(MENSAGEM_ERRO_REMOCAO);
+            }
+        });
+    }
+
+    private void mostraErro(String mensagem) {
+        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show();
     }
 
     private void configuraFabSalvaProduto() {
@@ -73,29 +90,38 @@ public class ListaProdutosActivity extends AppCompatActivity {
         new SalvaProdutoDialog(this, this::salva).mostra();
     }
 
-    private void salva(Produto produto) {
-        new BaseAsyncTask<>(() -> {
-            long id = dao.salva(produto);
-            return dao.buscaProduto(id);
-        }, produtoSalvo ->
-                adapter.adiciona(produtoSalvo))
-                .execute();
+    private void salva(Produto produtoCriado) {
+        repository.salva(produtoCriado, new ProdutoRepository.DadosCarregadosCallback<Produto>() {
+            @Override
+            public void quandoSucesso(Produto produtoSalvo) {
+                adapter.adiciona(produtoSalvo);
+            }
+
+            @Override
+            public void quandoFalha(String erro) {
+                mostraErro(MENSAGEM_ERRO_SALVA);
+            }
+        });
     }
 
     private void abreFormularioEditaProduto(int posicao, Produto produto) {
         new EditaProdutoDialog(this, produto,
-                produtoEditado -> edita(posicao, produtoEditado))
+                edita(posicao))
                 .mostra();
     }
 
-    private void edita(int posicao, Produto produto) {
-        new BaseAsyncTask<>(() -> {
-            dao.atualiza(produto);
-            return produto;
-        }, produtoEditado ->
-                adapter.edita(posicao, produtoEditado))
-                .execute();
+    @NotNull
+    private FormularioProdutoDialog.ConfirmacaoListener edita(int posicao) {
+        return produtoCriado -> repository.edita(produtoCriado, new ProdutoRepository.DadosCarregadosCallback<Produto>() {
+            @Override
+            public void quandoSucesso(Produto produtoEditado) {
+                adapter.edita(posicao, produtoEditado);
+            }
+
+            @Override
+            public void quandoFalha(String erro) {
+                mostraErro(MENSAGEM_ERRO_EDICAO);
+            }
+        });
     }
-
-
 }
